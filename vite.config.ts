ac0e121091ba2785511177309dd5e7652e153398
tucompadigital <devsecs@elcompa.digital>
2026-01-01 01:10:44 -0600

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
-	plugins: [cloudflare(), reactRouter()],
+	plugins: [
+		cloudflare(),
+		reactRouter(),
+	],
 });
