6266054fdede864899984c8d95422f8094310e57
tucompadigital <devsecs@elcompa.digital>
2026-01-01 01:39:05 -0600

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflare } from "@cloudflare/vite-plugin"; // [!code ++]
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [
-		cloudflare(), // [!code ++]
+		cloudflare(),
 		reactRouter(),
 		tsconfigPaths(),
 	],
--- workers/app.ts
+++ workers/app.ts
@@ -1,2 +1,14 @@
-// @ts-ignore
+import { createRequestHandler } from "react-router";
+
+// @ts-ignore - Vite will resolve this during 'npm run build'
 import * as build from "virtual:react-router/server-build";
+
+const requestHandler = createRequestHandler(build, import.meta.env.MODE);
+
+export default {
+	async fetch(request, env, ctx) {
+		return requestHandler(request, {
+			cloudflare: { env, ctx },
+		});
+	},
+} satisfies ExportedHandler;
