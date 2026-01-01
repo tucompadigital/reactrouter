7bd18e01ebccbdb449e33799016bd5ed3be2a98e
tucompadigital <devsecs@elcompa.digital>
2026-01-01 01:55:34 -0600

    commit

--- workers/app.ts
+++ workers/app.ts
@@ -1,12 +1,13 @@
 import { createRequestHandler } from "react-router";
-
-// @ts-ignore
+// @ts-ignore - Vite handles this virtual import
 import * as build from "virtual:react-router/server-build";
 
+// This creates the actual handler using the React Router build
 const requestHandler = createRequestHandler(build, import.meta.env.MODE);
 
 export default {
 	async fetch(request, env, ctx) {
+		// This passes the request and Cloudflare context (env, ctx) to React Router
 		return requestHandler(request, {
 			cloudflare: { env, ctx },
 		});
